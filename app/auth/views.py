1b4ce1848a8c2b63b9d761cac0699b1960df2062
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-08-07 20:54:05 +0300

    update template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from . import auth
-from flask import render_template, session
+from flask import render_template, session, Markup
 from .forms import RegisterForm, LoginForm
 from .. import db
 from flask import flash, url_for, redirect, request
@@ -9,26 +9,30 @@ from flask_login import login_user, login_required, logout_user
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    link = '<a href="auth/login">Login</a>'
     form = RegisterForm()
     if form.validate():
-        user = User.query.filter_by(email=form.email.data.lower()).first()
-        if user:
+        user = User.query.filter_by(email=form.email.data).first()
+        username = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
             flash('This email is already registered.')
+            return render_template('register.html', form=form, link=link)
+        if username is not None:
+            flash('This username is already in use')
             return render_template('register.html', form=form)
-        user = User(username=form.username.data, email=form.email.datalower(), password=form.password.data,
+        user = User(username=form.username.data, email=form.email.data.lower(), password=form.password.data,
                     name=form.name.data)
         db.session.add(user)
         db.session.commit()
-        flash('You have register')
-        return redirect(url_for('main.hello'))
-    return render_template('register.html', form=form)
+        flash('You have register.')
+    return render_template('register.html', form=form, link=link)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate:
-        user = User.query.filter_by(email=form.email.data.lower()).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             next = request.args.get('next')
