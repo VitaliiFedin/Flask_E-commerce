06af9cb55c178c6627d483931be7853ca7eefd9a
VitaliiFedin <vitalii.fedinn@gmail.com>
2023-11-01 23:48:01 +0200

    added pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,38 @@
-from flask import session, render_template
-from ..models import Product
+from flask import session, render_template, request
+from ..models import Product, Brand, Category
 from . import main
 
 
 @main.route('/', methods=['GET'])
-def hello():
+def home_page():
     print(session.items())
-    products = Product.query.filter(Product.stock > 0)
-    return render_template('products/index.html', products=products)
+    page = request.args.get('page', 1, type=int)
+    products = Product.query.filter(Product.stock > 0).paginate(page=page, per_page=2)
+    brands = Brand.query.join(Product, (Brand.id == Product.brand_id)).all()
+    categories = Category.query.join(Product, (Category.id == Product.category_id)).all()
+    return render_template('products/index.html', products=products, brands=brands, categories=categories)
+
+
+@main.route('/brand/<int:id>')
+def get_brand(id):
+    page = request.args.get('page', 1, type=int)
+    get_brand = Brand.query.filter_by(id=id).first_or_404()
+    brand = Product.query.filter_by(brand_id=get_brand.id).paginate(page=page, per_page=2)
+    brands = Brand.query.join(Product, (Brand.id == Product.brand_id)).all()
+    categories = Category.query.join(Product, (Category.id == Product.category_id)).all()
+    return render_template('products/index.html', brand=brand, brands=brands, categories=categories,
+                           get_brand=get_brand)
+
+
+@main.route('/category/<int:id>')
+def get_category(id):
+    page = request.args.get('page', 1, type=int)
+    get_cat = Category.query.filter_by(id=id).first_or_404()
+    category = Product.query.filter_by(category_id=get_cat.id).paginate(page=page, per_page=2)
+    categories = Category.query.join(Product, (Category.id == Product.category_id)).all()
+    brands = Brand.query.join(Product, (Brand.id == Product.brand_id)).all()
+    return render_template('products/index.html', category=category, categories=categories, brands=brands,
+                           get_cat=get_cat)
 
 
 
@@ -23,6 +48,3 @@ def check1():
     else:
         print('Bruh')
 """
-
-
-
